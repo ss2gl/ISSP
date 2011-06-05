60318b3259df8b5dfd05eb938f4003cea500e642
S Heritsch <admin@ss2gl.com>
2011-06-05 14:57:57 -0700

    Initial Commit

---
+++ ISSP/ISSPAppDelegate.h
@@ -0,0 +1,27 @@
+//
+//  ISSPAppDelegate.h
+//  ISSP
+//
+//  Created by Mac User on 6/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@class DetailViewController;
+
+@interface ISSPAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet UISplitViewController *splitViewController;
+
+@property (nonatomic, retain) IBOutlet RootViewController *rootViewController;
+
+@property (nonatomic, retain) IBOutlet DetailViewController *detailViewController;
+
+@end
---
+++ ISSP/RootViewController.h
@@ -0,0 +1,20 @@
+//
+//  RootViewController.h
+//  ISSP
+//
+//  Created by Mac User on 6/5/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+@interface RootViewController : UITableViewController {
+
+}
+
+		
+@property (nonatomic, retain) IBOutlet DetailViewController *detailViewController;
+
+@end
